97d36b9160d69c41e40dbd3244f0e1b3b2609573
Tinniam V Ganesh <tvganesh.85@gmail.com>
2016-07-01 11:26:48 +0530

    Docs

--- R/CHadamard.R
+++ R/CHadamard.R
@@ -5,6 +5,44 @@
 # This function computes  a controlled Hadamard Gate
 #
 ###########################################################################################
+
+#' @title
+#' controlled Hadamard Gate
+#'
+#' @description
+#' This function applies a controlled Hadamard gate om the input
+#'
+#' @usage
+#' CHadamard(q)
+#'
+#' @param q
+#' The input
+#'
+#' @return k
+#'
+#' @references
+#' \url{https://quantumexperience.ng.bluemix.net/}\cr
+#' \url{https://gigadom.wordpress.com/2016/06/23/introducing-qcsimulator-a-5-qubit-quantum-computing-simulator-in-r/}\cr
+#'
+#' @author
+#' Tinniam V Ganesh
+#' @note
+#' Maintainer: Tinniam V Ganesh \email{tvganesh.85@gmail.com}
+#'
+#' @examples
+#' \dontrun{
+#' CHadamard(q00_)
+#' CHadamard(I4)
+#' }
+#' @seealso
+#' \code{\link{CNOT2_01}}\cr
+#' \code{\link{PauliX}}\cr
+#'
+#' @import ggplot2
+#'
+#' @export
+#'
+
 CHadamard <- function (q){
     I=matrix(c(1,0,0,1),nrow=2,ncol=2)
 
@@ -38,5 +76,6 @@ CHadamard <- function (q){
     i=PauliX(SGate(Hadamard(TGate(I))))
     j = kronecker(I,i,"*")
     k = j %*% g1
+    k
 
 }
